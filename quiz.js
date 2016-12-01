dc189a4155d52126b1d39fb4bc5c0336d83d7b1a
joellegg <joellegg@bellsouth.net>
2016-12-01 14:38:05 -0600

    js to do outline

---
+++ quiz.js
@@ -0,0 +1,13 @@
+// Create a `tree` function that should build a pine tree out of a character in the Chrome dev tools console.
+
+// 1. Get reference to tree height from user input
+// 1. Get reference to character to make tree
+// 1. Get reference to button "grow your tree"
+
+// 2. function to make tree
+    // if either field is blank
+    // display alert to
+    // log tree to console
+
+// 3. Call function to run on button click with event listener
+// 3. Call function to run on return key press with event listener
