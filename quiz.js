19d532e29a30f8f68cec8966cd2186a796a9b799
joellegg <joellegg@bellsouth.net>
2016-12-01 15:04:36 -0600

    added reference to get inputs, added click event listener to run function

--- quiz.js
+++ quiz.js
@@ -1,13 +1,25 @@
 // Create a `tree` function that should build a pine tree out of a character in the Chrome dev tools console.
 
 // 1. Get reference to tree height from user input
+var treeHeight = document.getElementById('heightInput');
 // 1. Get reference to character to make tree
+var chara = document.getElementById('charInput');
 // 1. Get reference to button "grow your tree"
+var growButton = document.getElementById('growTree');
+
 
 // 2. function to make tree
+function makeTree () {
+    // log tree height to the console
+    console.log(treeHeight.value);
+    // log character to the console
+    console.log(chara.value);
     // if either field is blank
-    // display alert to
+    // display alert
     // log tree to console
+}
+
 
 // 3. Call function to run on button click with event listener
-// 3. Call function to run on return key press with event listener
+growButton.addEventListener('click', makeTree);
+// 3. Call function to run on return key press with event listener on either field
