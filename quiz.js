49be7e690c1bbfa6744ed294d70587741203fa9e
joellegg <joellegg@bellsouth.net>
2016-12-02 12:05:57 -0600

    pushed tree specs into function and object

--- quiz.js
+++ quiz.js
@@ -2,42 +2,49 @@
 
 // 1. Get reference to button "grow your tree"
 var growButton = document.getElementById('growTree');
+var treeHeight = document.getElementById('heightInput');
+var chara = document.getElementById('charInput');
 
 // run makeTree when enter key is pressed in input field
 function enterKeyPressed (e) {
-    var keyPressed = e.keyCode;
-    if (keyPressed === 13) {
-        makeTree();
+    var keyPressed = e.key;
+    if (keyPressed === "Enter") {
+        getSpecs();
     }
 }
 
-// 2. function to make tree
-function makeTree () {
+// set user input to an object
+function getSpecs () {
     // 1. Get reference to tree height from user input & log to console
     var treeHeight = document.getElementById('heightInput').value;
-
     // 1. Get reference to character to make tree & log to console
     var chara = document.getElementById('charInput').value;
+    var treeSpecs = {
+        height: treeHeight,
+        character: chara
+    };
+    makeTree(treeSpecs);
+}
 
+
+// 2. function to make tree
+function makeTree (treeSpecs) {
     // if either field is blank display alert
-    if (treeHeight === "" || chara === "") {
+    if (treeSpecs.height === "" || treeSpecs.character === "") {
         alert("Please enter values for both fields");
     }
-
     // for loop to get tree (hopefully)
-    for (var i = 0; i < treeHeight; i++) {
+    for (var i = 0; i < treeSpecs.height; i++) {
         var x = (2*i)+1;
-        var y = (treeHeight - i);
+        var y = (treeSpecs.height - i);
         var spaces = " ".repeat(y);
-        var xTimes = chara.repeat(x);
+        var xTimes = treeSpecs.character.repeat(x);
         console.log(spaces + xTimes);
     }
 }
 
 // 3. Call function to run on button click with event listener
-growButton.addEventListener('click', makeTree);
+growButton.addEventListener('click', getSpecs);
 // 3. Call function to run on return key press with event listener on either field
-var treeHeight = document.getElementById('heightInput');
-var chara = document.getElementById('charInput');
 treeHeight.addEventListener('keypress', enterKeyPressed);
 chara.addEventListener('keypress', enterKeyPressed);
